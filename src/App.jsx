f9955128ac9acc7398cac31f135b2bb1aefc36fc
Aditya Vishwakarma <adityavishwakarma1108@gmail.com>
2025-01-19 01:52:06 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+
+const App = () => {
+  return (
+    <main className="font-krona text-white bg-slate-950">
+      <Navbar />
+      <Hero />
+
+      {/* Marquee */}
+      {/* Projects */}
+      {/* Service */}
+      {/* ABout Agency */}
+      {/* CTA */}
+      {/* Footer */}
+    </main>
+  );
+};
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import { Link } from "lucide-react";
+import React from "react";
+
+const Navbar = () => {
+  const links = ["Service", "About", "Contact"];
+  return (
+    <nav className="flex justify-between items-center py-4 px-8 bg-transparent backdrop-blur-lg border-b-[1px] border-slate-200 border-opacity-30 ">
+      <div>
+        <h1 className="font-krona max-w-[10px] leading-4">Forma Fusion</h1>
+      </div>
+      <div className="flex space-x-4 font-krona">
+        {links.map((link, index) => (
+          <a key={index} href={`#${link.toLowerCase()}`}>
+            {link}
+          </a>
+        ))}
+      </div>
+      <div>
+        <Link />
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
